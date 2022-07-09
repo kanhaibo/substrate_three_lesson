0211e0d4bd317764a547062abf7e3949036dff19
kanhaibo <wealth_khb@126.com>
2022-07-09 21:23:55 +0800

    增加读取内容长度，个别浏览器512超过的问题

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,9 @@ fn main() {
             // 模式匹配当前流，成功则进行响应
             match stream {
                 // 成功的流执行，读取动作
-                Ok(stream) => hand_connection(stream),
+                Ok(stream) => {
+                    hand_connection(stream);
+                }
                 // 返回错误的原因
                 Err(e) => println!("the error {}", e.to_string()),
             }
@@ -26,21 +28,30 @@ fn main() {
         println!("error to bind port !")
     }
 }
+
 // 读取请求并响应
 fn hand_connection(mut stream: TcpStream) {
     // 声明存放数据的buffer
     let mut buffer = [0; 512];
+    // 判断是否读取完整
+    let mut done = true;
     // 读取内容到buffer
-    stream.read(&mut buffer).unwrap();
-    // 转化请求内容到字符串，并打印
-    println!("Request:{}", String::from_utf8_lossy(&buffer[..]));
-    // 返回连接内容
-    let contens = fs::read_to_string("hello.html").unwrap();
-    // 返回内容打印
-    println!("{}", contens);
+    while done {
+        let bytes_read = stream.read(&mut buffer).unwrap();
+        println!("{}", bytes_read);
+        // 返回内容打印
+        println!("Request:{}", String::from_utf8_lossy(&buffer[..]));
+        if bytes_read < 512 {
+            done = false;
+        }
+    }
     // 增加返回状态信息
+    let contens = fs::read_to_string("hello.html").unwrap();
+
     let response = format!("HTTP/1.1 200 OK \r \n \r \n {}", contens);
     //写入返回流
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
+
+    // 转化请求内容到字符串，并打印
 }
